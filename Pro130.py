130296d8ca9faf406b370ccad4071bf9064f3da9
Menal Kour <74865342+MenalKour@users.noreply.github.com>
2021-10-19 20:17:39 +0530

    Update Pro130.py

--- Pro130.py
+++ Pro130.py
@@ -4,6 +4,16 @@ import csv
 df = pd.read_csv("bright_stars.csv")
 del df["Luminosity"]
 df.to_csv("main.csv")
-
+df = pd.read_csv('bright_stars.csv')
+df
+df.columns
+final_data = df.dropna()
+final_data
+final_data.reset_index(drop=True,inplace = True)
+final_data
+final_data.to_csv('final_data.csv')
+final_data.describe()
+final_data.head(5)
+final_data.dtypes
 print(df.shape)
 print(list(df))
